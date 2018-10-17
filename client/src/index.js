3b02eadcc7487f0dbf23176b057736cb0e4911bc
Geoff Vincent <geoffkvincent@gmail.com>
2018-10-17 11:33:53 -0600

    redux refactor

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import App from './App';
+import store  from './store';
 import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(
