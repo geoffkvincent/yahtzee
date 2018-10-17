a90c767996498e30e2c4c5ddf720eeac560dde8f
Geoff Vincent <geoffkvincent@gmail.com>
2018-10-17 10:32:35 -0600

    import thunk

--- client/src/store.js
+++ client/src/store.js
@@ -1,8 +1,10 @@
-import { createStore } from 'redux'
+import { createStore, compose, applyMiddleware } from 'redux'
 import rootReducer from './reducers/index'
+import thunk from 'redux-thunk'
 
 const enhancers = compose(
-  
+  applyMiddleware(thunk),
+  windo.devToolsExtension && window.devToolsExtension
 )
 
 const store = createStore(rootReducer, {}, enhancers)
