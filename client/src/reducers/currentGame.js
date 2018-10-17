37306d9237c6a2366fd2c7abb9dc2f0d1eadf27b
Geoff Vincent <geoffkvincent@gmail.com>
2018-10-17 11:15:03 -0600

    togglekept func

--- client/src/reducers/currentGame.js
+++ client/src/reducers/currentGame.js
@@ -4,11 +4,30 @@ const ROLL_DICE = 'ROLL_DICE'
 const TOGGLE_KEPT = 'TOGGLE_KEPT'
 
 export const rollDice = () => {
+  return (dispatch, getState) => {
+    const { keep, dice } = getState().currentGame
 
+    const newDice = dice.map( (d, i) => {
+      if ( keepf.includes(i))
+      return Math.floor(Math.random() * 6) + 1
+    })
+
+    dispatch({ type: ROLL_DICE, dice: newDice})
+  }
 }
 
 export const toggleKept = (i) => {
+  return (dispatch, getState) => {
+    const { keep} = getState().currentGame
+    let updated
 
+    if (keep.includes(i))
+      updated = keep.filter( d => d !== i)
+    else
+      updated = [...keep, i]
+
+    dispatch({ type: TOGGLE_KEPT, keep: updated })
+  }
 }
 
 export default (
