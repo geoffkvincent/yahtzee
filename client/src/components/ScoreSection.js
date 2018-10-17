138f231d1e35af3073d7074d8b362c44a80d8591
Geoff Vincent <geoffkvincent@gmail.com>
2018-10-17 12:12:23 -0600

    scorecard

--- client/src/components/ScoreSection.js
+++ client/src/components/ScoreSection.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ScoreRow from './ScoreRow'
-import { List, Header, Divider, Segement } from 'semantic-ui-react'
-import {connect} from 'react-redux'
+import { List, Header, Divider, Segment } from 'semantic-ui-react'
+import { connect } from 'react-redux'
 
 class ScoreSection extends React.Component {
   total = (score, title) => {
@@ -12,8 +12,9 @@ class ScoreSection extends React.Component {
       </List.Item>
     )
   }
-  generateTotals =() => {
-    const { currentGame: {scores}, label } = this.props
+
+  generateTotals = () => {
+    const { currentGame: { scores }, label } = this.props
     const sectionScores = []
     const sectionTotal = scores
       .filter( s => s.section === label.toLowerCase())
@@ -25,21 +26,22 @@ class ScoreSection extends React.Component {
     sectionScores.push(this.total(sectionTotal, 'Section Total'))
 
     if (label === 'Upper') {
-      const bonus = sectionTotal >= 63? 35 : 0
-      sectionScores.push (this.total(bonus, 'Bonus'))
-      sectionScores.push (this.total(bonus + sectionTotal, 'Total Score'))
+      const bonus = sectionTotal >= 63 ? 35 : 0
+      sectionScores.push(this.total(bonus, 'Bonus'))
+      sectionScores.push(this.total(bonus + sectionTotal, 'Total Score'))
     }
 
     return sectionScores
 
   }
+
   render() {
-    const{ label, currentGame: { scores }} = this.props
+    const { label, currentGame: { scores } } = this.props
     return (
       <Segment basic>
-        <Header as='h3'>{label} Section</Header>
+        <Header as="h3">{label} Section</Header>
         <List divided>
-          { scores.filter(s => s.section === label.tolowerCase()).map((score, i) => {
+          { scores.filter(s => s.section === label.toLowerCase() ).map( (score, i) => {
               return <ScoreRow key={i} {...score} />
             })
           }
@@ -50,8 +52,8 @@ class ScoreSection extends React.Component {
   }
 }
 
-const mapStateToProps = (state) => {
-  return {currentGame: state.currentGame}
+const  mapStateToProps = (state) => {
+  return { currentGame: state.currentGame }
 }
 
 export default connect(mapStateToProps)(ScoreSection)
