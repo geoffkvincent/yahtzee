bc7eceec0a2e4027ee7f025516b6ed837460314b
Geoff Vincent <geoffkvincent@gmail.com>
2018-10-17 12:19:47 -0600

    added scores

---
+++ client/src/components/ScoreRow.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import connect form 'react-redux'
+import seman
+import styled
+
+const Pointer = styled(List.Icon) `
+  cursor: pointer;
+`
+
+class ScoreRow extends React.Component {
+  render () {
+    return (
+      <List.Item>
+        { score === null &&
+            <Pointer 
+              name="check circle outline"
+              color="green"
+            />
+        }
+        <List.Content>
+        <Header as="h4" floated="left">{score || 0}</Header>
+        <Header as="h5" floated="left">{score || 0}</Header>
+        </List.Content>
+      </List.Item>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return { currentGame: state.currentGame }
+}
+
+export default connect(mapStateToProps)(ScoreRow)
