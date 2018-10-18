f8400dc00afbb98863e58b9956696bf741eafd5c
Geoff Vincent <geoffkvincent@gmail.com>
2018-10-18 09:42:59 -0600

    score card func

---
+++ client/src/utils/scoringEngine.js
@@ -0,0 +1,35 @@
+export const singles = (value, dice) => {
+
+}
+
+export const addAllDice = (type, dice) => {
+
+}
+
+export const staticScore = (type, dice) => {
+
+}
+
+const validateFullHouse = (dice) => {
+
+}
+
+const validateLowStraight = (dice) => {
+
+}
+
+const validateHighStraight = (dice) => {
+
+}
+
+const validateYahtzee = (dice) => {
+
+}
+
+const validateThreeOfAKind = (dice) => {
+
+}
+
+const validateFourOfAKind = (dice) => {
+  
+}
